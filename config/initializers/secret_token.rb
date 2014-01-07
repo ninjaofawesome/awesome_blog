b5c0666c7b90bd4fd8f88d229bfb6dacd4c63b24
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-01-07 17:43:41 -0500

    @post.title and @post.body not working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwesomeBlog::Application.config.secret_key_base = '0d79ffbe1159853f37936fc5957b8e7e6efc076e0a583d3c72b6c1bbbba2f9c345ad70458ef2dc07cc0c1329333bf165de66b2f905ca6b2b61d4f79fc064ed7e'
